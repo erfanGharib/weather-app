ddb7bca046d7ec7ae7b6fedbf5cabb43ecb0bc9c
erfan gharib <erfangharib5@gmail.com>
2022-02-06 22:35:23 +0330

    update: index.html - favicon - gitignore - package.json - tailwind config file - styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,19 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        163: '40rem'
+        163: '40rem',
+      },
+      height: {
+        38: '9.5rem'
       },
       backgroundSize: {
-	full: '100%'
+	      full: '100%'
+      },
+      minWidth: {
+        4:'1rem'
+      },
+      minHeight: {
+        4:'1rem'
       }
     },
   },
