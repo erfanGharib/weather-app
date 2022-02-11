9538590d824c3e4ad544ee895ad1c5d2dbe9ad2d
erfan gharib <erfangharib5@gmail.com>
2022-02-11 20:28:19 +0330

    change location to 'stateless functional component'

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,70 +1,31 @@
-import React, { Component } from 'react';
-import cities from 'cities.json';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
-class Location extends Component {
-    constructor() {
-        super();
-        this.inputRef = React.createRef();
-    }
-    render() {
-        return (
-            <div className='w-full flex-col-reverse h-24 flex mb-6 justify-between items-center'>
-                <div className='flex items-center mx-auto mt-4'>
-                    <i className='ico location'></i>
-                    <span className='text-xl ml-1 font-medium capitalize'>shahrud</span>
-                </div>
-
-                <form className='relative flex flex-col h-full w-full'>
-                    <input
-                        type="search"
-                        name="city-search"
-                        id="city-search"
-                        className='h-12 rounded-lg w-full px-3 bg-white bg-opacity-80 outline-none text-base text-gray-900 placeholder:text-gray-900 placeholder:opacity-30'
-                        placeholder='Search Citys...'
-                        ref={this.inputRef}
-                        onInput={this.citySearch}
-                    />
-                    <i className='bg-cover w-6 h-6 search absolute top-3 opacity-50 right-3'></i>
-
-                    <ul id='search-result' className='z-10 w-full absolute top-14 rounded-lg shadow-2xl max-h-56 overflow-hidden flex flex-col'>
-                    </ul>
-                </form>
+const Location = () => {
+    return (
+        <div className='w-full flex-col-reverse h-24 flex mb-6 justify-between items-center'>
+            <div className='flex items-center mx-auto mt-4'>
+                <i className='ico location'></i>
+                <span className='text-xl ml-1 font-medium capitalize'>{this.props.currentCity}</span>
             </div>
-        );
-    }
 
-    citySearch = () => {
-        this.props.updateWeather();
-        cities.forEach((n, index, arr) => {
-            if (n.name.toLowerCase().startsWith(this.inputRef.current.n))
-                ReactDOM.render(
-                    <li
-                        className='search-result-child'
-                        onClick={this.setState({ currentCity: n.name })}
-                        key={index}
-                    >
-                        {n.name}
-                    </li>,
-                    document.querySelector('#search-result')
-                )
-            if (index === 100)
-                return;
-        })
-    }
-    // showSearchResult(n,index) {
-    //     [1,2,3,4].map(()=>{
-    //         return (
-    //             <li
-    //                 className='search-result-child' 
-    //                 onClick={this.setState({currentCity: n.name})} 
-    //                 key={index}
-    //             >
-    //                 {n.name}
-    //             </li>
-    //         )
-    //     })
-    // }
+            <form className='relative flex flex-col h-full w-full'>
+                <input
+                    type="text"
+                    id="city-search"
+                    className='h-12 bg-trimmedWhite rounded-t-lg w-full px-4 outline-none text-base text-gray-600 placeholder:text-gray-600 placeholder:opacity-30'
+                    placeholder='Search Citys...'
+                    ref={this.props.inputRef}
+                    spellCheck='false'
+                    autoComplete="off"
+                    onInput={this.props.onSearch}
+                />
+                <i className='bg-cover w-6 h-6 search absolute top-3 opacity-50 right-3'></i>
+
+                <ul id='search-result' className='z-10 rounded-b-lg w-full absolute top-12 max-h-56 overflow-hidden flex flex-col'>
+                </ul>
+            </form>
+        </div>
+    );
 }
 
 export default Location;
