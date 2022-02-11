26682e59de0dc3144e5837bcb1908829c27c327d
erfan gharib <erfangharib5@gmail.com>
2022-02-11 20:31:16 +0330

    add: dayNames array - day list ref into state - current day update: use object destructering for component props - loop loading day list

--- src/components/weather-properties.jsx
+++ src/components/weather-properties.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 const dayNames = [
     'saturday',
     'sunday',
@@ -7,53 +7,71 @@ const dayNames = [
     'wednesday',
     'tuesday',
     'friday'
-]
-const WeatherProperties =props=> {
-    return (
-        <div className='w-full h-full flex-col flex justify-between items-center'>
-            <div className='flex flex-col justify-center items-center w-full'>
-                <span className='bg-center opacity-90 snowy bg-contain block w-40 h-28 bg-no-repeat'></span>
-                
-                <span id='weather-text' className='capitalize font-medium text-3xl'>{props.weatherText}</span>
-                
-                <div className='flex items-center justify-between w-44'>
-                    <span className='text-3xl font-light'>{props.tempC}<b className='text-xl'>C</b></span>
-                    <span className='text-3xl font-light'>{props.tempF}<b className='text-xl'>F</b></span>
-                </div>
+];
 
-                <div className='flex items-center justify-between w-44'>
-                    <span id='humidity' title='humidity' className='items-center flex'>
-                        <i className='ico mr-2 items-center humidity'></i>
-                        <p className='font-light'>{props.humidity}</p>
-                    </span>
-                    <span id='wind-speed' title='wind speed' className='flex items-center'>
-                        <i className='ico mr-2 items-center wind-speed'></i>
-                        <p className='font-light'>{props.windSpeed}</p>
-                    </span>
+class WeatherProperties extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            days: [
+                React.createRef,
+                React.createRef,
+                React.createRef,
+            ],
+            currentDay: new Date().getDay(),
+        }
+    }
+
+    render() {
+        const {tempF, tempC, weatherText, humidity, windSpeed, time, date_, onDayChange} = this.props;
+        return (
+            <div className='w-full h-full flex-col flex justify-between items-center'>
+                <div className='flex flex-col justify-center items-center w-full'>
+                    <span className='bg-center opacity-90 snowy bg-contain block w-40 h-28 bg-no-repeat'></span>
+                    
+                    <span id='weather-text' className='capitalize font-medium text-3xl'>{weatherText}</span>
+                    
+                    <div className='flex items-center justify-between w-44'>
+                        <span className='text-3xl font-light'>{tempC}<b className='text-xl'>C</b></span>
+                        <span className='text-3xl font-light'>{tempF}<b className='text-xl'>F</b></span>
+                    </div>
+    
+                    <div className='flex items-center justify-between w-44'>
+                        <span id='humidity' title='humidity' className='items-center flex'>
+                            <i className='ico mr-2 items-center humidity'></i>
+                            <p className='font-light'>{humidity}</p>
+                        </span>
+                        <span id='wind-speed' title='wind speed' className='flex items-center'>
+                            <i className='ico mr-2 items-center wind-speed'></i>
+                            <p className='font-light'>{windSpeed}</p>
+                        </span>
+                    </div>
+                    
+                    <div className='flex items-center w-44 justify-between'>
+                        <p className='font-light'>{time}</p>
+                        <p className='font-light'>{date_}</p>
+                    </div>
                 </div>
-                
-                <div className='flex items-center w-44 justify-between'>
-                    <p className='font-light'>{props.time}</p>
-                    <p className='font-light'>{props.date_}</p>
-                </div>
+    
+                <ul
+                    id='day-of-week'
+                    className='relative text-opacity-90 flex flex-col-reverse my-auto items-center overflow-hidden'
+                >
+                    {this.state.days.map((value,li)=>
+                        <li 
+                            key={li}
+                            ref={value}
+                            onClick={()=>onDayChange(li)}
+                            className='cursor-pointer capitalize font-bold text-3xl opacity-50'
+                        >
+                            {dayNames[this.state.currentDay+li]}
+                        </li>
+                    )}
+                </ul>
             </div>
-
-            <ul
-                id='day-of-week'
-                className='relative text-opacity-90 flex flex-col my-auto items-center overflow-hidden'
-            >
-                <li id='selected-day' className='capitalize text-4xl font-bold'>
-                    {dayNames[props.selectedDay]}
-                </li>
-                <li id='selected-day' className='capitalize text-3xl opacity-50 font-medium'>
-                    {dayNames[props.selectedDay+1]}
-                </li>
-                <li id='selected-day' className='capitalize text-3xl opacity-50 font-medium'>
-                    {dayNames[props.selectedDay+2]}
-                </li>
-            </ul>
-        </div>
-    );
+        );
+    }
 }
 
 export default WeatherProperties;
