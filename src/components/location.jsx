dee7ace0ca61c95a359e4a1c6ac4ef556263393a
erfan gharib <erfangharib5@gmail.com>
2022-02-14 11:27:41 +0330

    update location component

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Location = () => {
+const Location = props => {
     return (
         <div className='w-full flex-col-reverse h-24 flex mb-6 justify-between items-center'>
             <div className='flex items-center mx-auto mt-4'>
                 <i className='ico location'></i>
-                <span className='text-xl ml-1 font-medium capitalize'>{this.props.currentCity}</span>
+                <span className='text-xl ml-1 font-medium capitalize'>{props.currentCity}</span>
             </div>
 
-            <form className='relative flex flex-col h-full w-full'>
+            <form className='relative flex flex-col z-10 h-full opacity-95 w-full'>
                 <input
                     type="text"
                     id="city-search"
-                    className='h-12 bg-trimmedWhite rounded-t-lg w-full px-4 outline-none text-base text-gray-600 placeholder:text-gray-600 placeholder:opacity-30'
+                    className='h-12 bg-trimmedWhite rounded-lg rounded-t-lg w-full px-4 outline-none text-base text-gray-600 placeholder:text-gray-600 placeholder:opacity-30'
                     placeholder='Search Citys...'
-                    ref={this.props.inputRef}
+                    ref={props.inputRef}
                     spellCheck='false'
                     autoComplete="off"
-                    onInput={this.props.onSearch}
+                    onInput={props.onSearch}
                 />
                 <i className='bg-cover w-6 h-6 search absolute top-3 opacity-50 right-3'></i>
 
