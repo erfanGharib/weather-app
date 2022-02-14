4a11de3108deed9cc2a9facd9d9cce6dfd757c8d
erfan gharib <erfangharib5@gmail.com>
2022-02-14 11:30:44 +0330

    fix: day change - call selectDay when component mounted - put this.props in object destructor

--- src/components/weather-properties.jsx
+++ src/components/weather-properties.jsx
@@ -12,25 +12,41 @@ const dayNames = [
 class WeatherProperties extends Component {
     constructor(props) {
         super(props);
-        
-        this.state = {
-            days: [
-                React.createRef,
-                React.createRef,
-                React.createRef,
-            ],
-            currentDay: new Date().getDay(),
-        }
+        this.days = [
+            React.createRef(),
+            React.createRef(),
+            React.createRef(),
+        ];
+    }
+    
+    componentDidMount() {
+        this.selectDay(0);
+    }
+
+    selectDay =(li_index)=> {
+        this.days.forEach(value=>value.current.id='');
+        this.days[li_index].current.id='selected-day';
     }
 
     render() {
-        const {tempF, tempC, weatherText, humidity, windSpeed, time, date_, onDayChange} = this.props;
+        const {
+            tempF, tempC, weatherText, 
+            humidity, windSpeed, time, date_, 
+            onDayChange
+        } = this.props;
+        const weatherIconCssClass = `bg-center opacity-90 ${weatherText.bg_icoText} bg-contain block w-40 h-28 bg-no-repeat`
+        
         return (
             <div className='w-full h-full flex-col flex justify-between items-center'>
                 <div className='flex flex-col justify-center items-center w-full'>
-                    <span className='bg-center opacity-90 snowy bg-contain block w-40 h-28 bg-no-repeat'></span>
+                    <span className={weatherIconCssClass}></span>
                     
-                    <span id='weather-text' className='capitalize font-medium text-3xl'>{weatherText}</span>
+                    <span 
+                        id='weather-text' 
+                        className='capitalize font-medium text-3xl'
+                    >
+                        {weatherText.apiText}
+                    </span>
                     
                     <div className='flex items-center justify-between w-44'>
                         <span className='text-3xl font-light'>{tempC}<b className='text-xl'>C</b></span>
@@ -54,21 +70,25 @@ class WeatherProperties extends Component {
                     </div>
                 </div>
     
-                <ul
+                <div
                     id='day-of-week'
-                    className='relative text-opacity-90 flex flex-col-reverse my-auto items-center overflow-hidden'
+                    className='relative text-opacity-90 flex flex-col my-auto items-center'
                 >
-                    {this.state.days.map((value,li)=>
-                        <li 
+                    {this.days.map((value,li)=>
+                        <button 
                             key={li}
                             ref={value}
-                            onClick={()=>onDayChange(li)}
-                            className='cursor-pointer capitalize font-bold text-3xl opacity-50'
+                            onClick={()=>{
+                                onDayChange(li);
+                                this.selectDay(li);
+                            }}
+                            className='cursor-pointer transition duration-150 transform hover:scale-110 capitalize font-bold text-3xl opacity-50'
                         >
-                            {dayNames[this.state.currentDay+li]}
-                        </li>
+                            {dayNames[this.props.currentDay+li]}
+                            <small className='text-lg font-light'>{li===0 ? '(today)' : ''}</small>
+                        </button>
                     )}
-                </ul>
+                </div>
             </div>
         );
     }
