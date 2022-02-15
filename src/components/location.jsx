e608cae0e7477f3480af142ef71f9fa3d851404d
erfan gharib <erfangharib5@gmail.com>
2022-02-15 23:37:37 +0330

    update: index.html - location component

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -8,7 +8,7 @@ const Location = props => {
                 <span className='text-xl ml-1 font-medium capitalize'>{props.currentCity}</span>
             </div>
 
-            <form className='relative flex flex-col z-10 h-full opacity-95 w-full'>
+            <div className='relative flex flex-col z-10 h-full opacity-95 w-full'>
                 <input
                     type="text"
                     id="city-search"
@@ -17,13 +17,17 @@ const Location = props => {
                     ref={props.inputRef}
                     spellCheck='false'
                     autoComplete="off"
-                    onInput={props.onSearch}
                 />
-                <i className='bg-cover w-6 h-6 search absolute top-3 opacity-50 right-3'></i>
+                <button 
+                    onClick={props.onSearch} 
+                    className='w-12 h-full hover:bg-white rounded-lg absolute right-0 opacity-50'
+                >
+                    <i className='w-6 h-6 bg-cover m-auto search block'></i>
+                </button>
 
                 <ul id='search-result' className='z-10 rounded-b-lg w-full absolute top-12 max-h-56 overflow-hidden flex flex-col'>
                 </ul>
-            </form>
+            </div>
         </div>
     );
 }
